2079e0d176fb5b02741e15c2bfad17b705f9eb9e
mm909 <Bobar312@gmail.com>
2018-11-23 23:59:52 -0800

    Updated to the new p5.js format

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 const TOTAL = 500;
 let birds = [];
 let pipes = [];
@@ -12,16 +11,18 @@ let gen = 0;
 let NNV;
 let hs = 0;
 
-function PickNewExample(j){
-    let temp = floor(random(0,j));
-    if(temp < 0) {
-      temp = 0;
-    }
-    ExampleBird = birds[temp];
+function PickNewExample(j) {
+  let temp = floor(random(0, j));
+  if (temp < 0) {
+    temp = 0;
+  }
+  ExampleBird = birds[temp];
 }
 
 function setup() {
-  createCanvas(Width, Height);
+  var canvas = createCanvas(Width, Height);
+  canvas.parent("canvasContainer")
+  // createCanvas(Width, Height);
   slider = createSlider(1, 100, 1);
   for (let i = 0; i < TOTAL; i++) {
     birds[i] = new Bird();
@@ -44,15 +45,15 @@ function draw() {
       pipes[i].update();
 
       for (let j = birds.length - 1; j >= 0; j--) {
-        if(birds[j].y >= Height || birds[j].y <= 0){
+        if (birds[j].y >= Height || birds[j].y <= 0) {
           savedBirds.push(birds.splice(j, 1)[0]);
-            if(j >= 0){
-              PickNewExample(j-1);
-            }
+          if (j >= 0) {
+            PickNewExample(j - 1);
+          }
         } else if (pipes[i].hits(birds[j])) {
           savedBirds.push(birds.splice(j, 1)[0]);
-            if(j >= 0){
-              PickNewExample(j-1);
+          if (j >= 0) {
+            PickNewExample(j - 1);
           }
         }
       }
@@ -85,13 +86,13 @@ function draw() {
   NNV.birdx = ExampleBird.x;
   NNV.birdy = ExampleBird.y;
 
-  NNV.setValues(960,240,642,482,30,30);
+  NNV.setValues(960, 240, 642, 482, 30, 30);
   NNV.draw();
   fill(255)
-  if(birds[0].score > hs){
+  if (birds[0].score > hs) {
     hs = birds[0].score;
   }
-  text("Current Score: " + birds[0].score, 650,475)
-  text("High Score: " + hs, 650,25)
-  text("Generation: " + gen, width - 150,25)
+  text("Current Score: " + birds[0].score, 650, 475)
+  text("High Score: " + hs, 650, 25)
+  text("Generation: " + gen, width - 150, 25)
 }
